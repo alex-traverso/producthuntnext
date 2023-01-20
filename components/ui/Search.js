db2df7c1be7df87f530e73e806d53746e5f4de66
alex traverso <alextraverso6@gmail.com>
2023-01-20 11:43:59 -0300

    add searchProduct function in search input

--- components/ui/Search.js
+++ components/ui/Search.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Search = () => {
-return (
-    <div className='input-nav-container'>
-        <input type="text" placeholder='Buscar Productos' className='search-input'/>
-        <button type='submit' className='submit-input'></button>
-    </div>
-)
-}
+  const [search, setSearch] = useState("");
+
+  const searchProduct = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
+  return (
+    <>
+      <div className='input-nav-container'>
+        <form onSubmit={searchProduct}>
+          <input
+            type='text'
+            placeholder='Buscar Productos'
+            className='search-input'
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button type='submit' className='submit-input'></button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Search;
